977d4a80632637e458bbb7a02a49ce04847548c5
Riya Singhal <54725441+Riya-Singhal@users.noreply.github.com>
2022-06-22 14:27:45 +0530

    Update prevmain.go

--- prevmain.go
+++ prevmain.go
@@ -7,9 +7,40 @@ import (
 )
 
 // panic(err) is just used for example purposes.
-func Prevmn() {
+func Prevmain(){
 	// Key and tweak should be byte arrays. Put your key and tweak here.
 	// To make it easier for demo purposes, decode from a hex string here.
-	fmt.Println("heyyy chl rha a")
+	key, err := hex.DecodeString("EF4359D8D580AA4F7F036D6F04FC6A94")
+	if err != nil {
+		panic(err)
+	}
+	tweak, err := hex.DecodeString("D8E7920AFA330A73")
+	if err != nil {
+		panic(err)
+	}
 
+	// Create a new FF1 cipher "object"
+	// 10 is the radix/base, and 8 is the tweak length.
+	FF1, err := ff1.NewCipher(10, 8, key, tweak)
+	if err != nil {
+		panic(err)
+	}
+
+	original := "123456789"
+
+	// Call the encryption function on an example SSN
+	ciphertext, err := FF1.Encrypt(original)
+	if err != nil {
+		panic(err)
+	}
+
+	plaintext, err := FF1.Decrypt(ciphertext)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Original:", original)
+	fmt.Println("Ciphertext:", ciphertext)
+	fmt.Println("Plaintext:", plaintext)
+	return 0
 }
